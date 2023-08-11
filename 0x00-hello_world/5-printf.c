82045267a69e29320d12fe1fa160ea3b7305af72
yaw1212 <bartimeaustettey137@gmail.com>
2023-08-11 07:14:26 -0700

    Print sizes of data types

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printint output with printf
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+[
+	printf("with proper grammar, but the outcome is a piece of art,\n")];
+
