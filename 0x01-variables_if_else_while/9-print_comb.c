d5dd68907ada10a04fcb38a0f69569aa19bdaf22
yaw1212 <bartimeaustettey137@gmail.com>
2023-08-14 14:12:07 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: always 0 (Sucess)
+ */
+
+
+int main(void)
+
+{
+
+	int d;
+
+
+	for  (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+
+	if (d != '9')
+        	{
+		putchar(,);
+              	putchar(' ')
+	       	}
+        }
+	putchar('\n');
+
+
+	return (0);
+
+}
