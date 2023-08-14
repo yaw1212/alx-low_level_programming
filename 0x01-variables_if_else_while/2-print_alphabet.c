d5c089fd7d9429cd34b07ac1e79ef576f8f478f8
yaw1212 <bartimeaustettey137@gmail.com>
2023-08-14 06:49:40 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabetic
+ * Return: Always (Sucess)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar (c);
+	        putchar('\n');
+	}
+
+	return (0);
+
+}
