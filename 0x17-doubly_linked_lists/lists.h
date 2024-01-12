df664dd9fab0b209a7355da7b300fd94b23a6e32
Yaw1212 <baronchase6@gmail.com>
2024-01-12 11:49:56 -0800

    Task 0 done

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,44 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/* Standard Libreries */
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * struct dlistint_s - doubly linked list
+  * @n: integer
+  * @prev: points to the previous node
+  * @next: points to the next node
+  *
+  * Description: doubly linked list node structure
+  *
+  */
+
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+/* Task 0 */
+size_t print_dlistint(const dlistint_t *h);
+/* Task 1 */
+size_t dlistint_len(const dlistint_t *h);
+/* Task 2 */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+/* Task 3 */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+/* Task 4 */
+void free_dlistint(dlistint_t *head);
+/* Task 5 */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+/* Task 6 */
+int sum_dlistint(dlistint_t *head);
+/* Task 7 */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+/* Task 8 */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+
+#endif /* LISTS_H */
