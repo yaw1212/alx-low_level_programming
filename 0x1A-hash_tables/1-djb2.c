f042171139bd9f8619b2cf509d11800a1b757746
Yaw1212 <baronchase6@gmail.com>
2024-02-02 17:34:19 -0800

    Integrate djb2 hash algorithm
    Add the djb2 hash algorithm implementation for the hash function. This widely-used hashing algorithm is known for its simplicity and effectiveness in generating hash values for strings. The djb2 algorithm is a non-cryptographic hash function created by Daniel J. Bernstein and is implemented to provide efficient and uniform distribution of hash keys in the hash table data structure.

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,12 +1,24 @@
+#include "hash_tables.h"
+
+/* Task 1. djb2 */
+
+/**
+  * hash_djb2 - Implements the djb2 hash algorithm
+  *
+  * @str: The string to hash
+  *
+  * Return: The hash value
+  */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
